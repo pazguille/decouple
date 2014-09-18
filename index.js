b1a8defbdc06a684dccd796a968b9d7ce87cdaed
Guille Paz <guille87paz@gmail.com>
2014-09-18 08:45:42 -0300

    Create decouple module.

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+}());
+
+function decouple(node, event, fn) {
+  var eve,
+      tracking = false;
+
+  function captureEvent(e) {
+    eve = e;
+    track();
+  }
+
+  function track() {
+    if (!tracking) {
+      requestAnimFrame(update);
+      tracking = true;
+    }
+  }
+
+  function update() {
+    fn.call(node, eve);
+    tracking = false;
+  }
+
+  node.addEventListener(event, captureEvent, false);
+}
+
+/**
+ * Expose decouple
+ */
+module.exports = decouple;
