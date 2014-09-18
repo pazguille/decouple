3491ae0aa1fb145292a10ba54e4b67e5b751ac90
Guille Paz <guille87paz@gmail.com>
2014-09-18 08:47:05 -0300

    Build decouple v0.0.1

---
+++ dist/decouple.js
@@ -0,0 +1,58 @@
+/*!
+ * decouple - v0.0.1
+ *
+ * Copyright (c) 2014, @pazguille <guille87paz@gmail.com>
+ * Released under the MIT license.
+ */
+(function(window) {
+'use strict';
+
+var requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+}());
+
+function decouple(node, event, fn) {
+  var eve,
+      tracking = false;
+
+  function captureEvent(e) {
+    eve = e;
+    track();
+  }
+
+  function track() {
+    if (!tracking) {
+      requestAnimFrame(update);
+      tracking = true;
+    }
+  }
+
+  function update() {
+    fn.call(node, eve);
+    tracking = false;
+  }
+
+  node.addEventListener(event, captureEvent, false);
+}
+
+/**
+ * Expose decouple
+ */
+// AMD
+if (typeof window.define === 'function' && window.define.amd !== undefined) {
+  window.define('decouple', [], function () {
+    return decouple;
+  });
+// CommonJS
+} else if (typeof module !== 'undefined' && module.exports !== undefined) {
+  module.exports = decouple;
+// Browser
+} else {
+  window.decouple = decouple;
+};
+
+}(this));
