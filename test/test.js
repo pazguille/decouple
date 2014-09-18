acc9b461f306ac3370428536eb96b6de32db44fd
Guille Paz <guille87paz@gmail.com>
2014-09-18 08:46:31 -0300

    Create decouple tests.

---
+++ test/test.js
@@ -0,0 +1,36 @@
+if (typeof window === 'undefined') {
+  var decouple = require('../'),
+      assert = require('better-assert');
+}
+
+decouple(window, 'scroll', function(eve) {
+  document.body.setAttribute('data-scroll', window.pageYOffset);
+});
+
+describe('decouple', function () {
+
+  it('should be defined.', function () {
+    assert(decouple !== undefined);
+  });
+
+  it('should be a function.', function () {
+    assert(typeof decouple === 'function');
+  });
+
+  it('should execute a given function on a given DOM event.', function (done) {
+    window.scrollTo(0, 500);
+
+    setTimeout(function() {
+      assert(window.pageYOffset == 500);
+      assert(document.body.getAttribute('data-scroll') === '500');
+      done();
+    }, 200);
+
+    setTimeout(function() {
+      window.scrollTo(0, 0);
+    }, 300);
+
+
+  });
+
+});
